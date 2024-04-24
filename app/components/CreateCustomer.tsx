bc3f15616c28a382cca3d344daa47c89200d945a
oxalcvasquez <oxalc0101@gmail.com>
2024-04-23 20:11:09 -0500

    Defining CreateCustomer Component

---
+++ app/components/CreateCustomer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { RiUserAddFill } from "react-icons/ri";
+
+export const CreateCustomer = () => {
+  return (
+    <button className="btn btn-primary w-full">Registrar nuevo cliente <RiUserAddFill className="ml-2" size={18} /></button>
+
+  )
+}
+export default CreateCustomer;
